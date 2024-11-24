15a77297a8918b47aa1c01327d853944702af965
gelvetica <helveticahelvetica@protonmail.com>
2024-11-23 21:00:16 -0500

    add suspense or something

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,11 +3,10 @@
 import {useSearchParams} from "next/navigation";
 import {callbackCallbackGet} from "@/lib/client";
 import useSWR from "swr";
+import { Suspense } from 'react'
 
-function sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-export default function SignIn() {
+
+function SignIn() {
     const searchParams = useSearchParams()
     const input_code = searchParams.get('code')
     const {data, error, isLoading} = useSWR({code: input_code}, callbackCallbackGet);
@@ -15,4 +14,17 @@ export default function SignIn() {
     if (isLoading) return <div>please wait...</div>
     localStorage.setItem("strudel_token", data.access_token)
     window.location.href = "/userlandingredir"
+}
+
+function wtf() {
+    return <div>U have no idea how much im crying right now</div>
+}
+
+export default function SignInPage() {
+    return (
+        // You could have a loading skeleton as the `fallback` too
+        <Suspense fallback={wtf()}>
+            <SignIn />
+        </Suspense>
+    )
 }
